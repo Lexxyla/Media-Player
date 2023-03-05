042a4630cf17097841b44f848f52022fc90a4b3c
lexxyla <bamideleadefolaju@gmail.com>
2023-03-06 00:11:46 +0100

    init

--- playlister/app.py
+++ playlister/app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# OUR MOCK ARRAY OF PROJECTS
+@app.route('/')
+def index():
+	"""Return homepage."""
+	return render_template('home.html', msg='Hello, world!')
+
 playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+		{ 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+		{ 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+		]
 
 @app.route('/playlists')
 def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', 'playlists=playlists')
+	"""Show all playlists."""
+	return render_template('playlists_index.html', playlists='playlists')
