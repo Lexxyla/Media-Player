c9bc459305ee2ad5cbd3f72a706ec810cca92fdb
lexxyla <bamideleadefolaju@gmail.com>
2023-03-06 00:50:34 +0100

    Users can see all playlists

--- playlister/app.py
+++ playlister/app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	"""Return homepage."""
-	return render_template('home.html', msg='Hello, world!')
+#@app.route('/')
+#def index():
+#	"""Return homepage."""
+#	return render_template('home.html', msg='Hello, world!')
 
 playlists = [
 		{ 'title': 'Cat Videos', 'description': 'Cats acting weird' },
 		{ 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 		]
 
-@app.route('/playlists')
+@app.route('/')
 def playlists_index():
 	"""Show all playlists."""
-	return render_template('playlists_index.html', playlists='playlists')
+	return render_template('playlists_index.html', playlists=playlists)
