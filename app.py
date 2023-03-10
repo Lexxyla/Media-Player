72d7d7d2a714625b7b3bc3beed8c5b95a8217482
Bamidele Adefolaju <bamideleadefolaju@gmail.com>
2023-03-10 14:04:10 +0100

    modified:   ../app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
@@ -19,26 +20,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def playlists_index():
-	"""Show all playlists."""
-	return render_template('playlists_index.html', playlists=playlists.find())
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 @app.route('/playlists/new')
 def playlists_new():
-	"""Create a new playlist."""
-	return render_template('playlists_new.html')
+    """Create a new playlist."""
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-#    """Submit a new playlist."""
-#    print(request.form.to_dict())
-#    return redirect(url_for('playlists_index'))
-
-#@app.route('/playlists', methods=['POST'])
-#def playlists_submit():
     """Submit a new playlist."""
     playlist = {
         'title': request.form.get('title'),
-        'description': request.form.get('description')
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
-    playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    print(playlist)
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
